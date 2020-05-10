d631b5624c1b0eaa9d4850665a84c6c0136e1213
Nadia van Leur <nadiavl@hotmail.com>
2020-05-10 20:16:39 +0200

    build

---
+++ build/precache-manifest.851d89151d64686c7937d598c98907e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc61611ad15ebdfac0e4ac6e1c2d30bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f7414ee0b2f810bb06b",
+    "url": "/static/css/main.7e040639.chunk.css"
+  },
+  {
+    "revision": "0647729fc4472ba9cdf6",
+    "url": "/static/js/2.fd98aa2e.chunk.js"
+  },
+  {
+    "revision": "7eef9a85742038dd10bb20e4f098ac69",
+    "url": "/static/js/2.fd98aa2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7414ee0b2f810bb06b",
+    "url": "/static/js/main.90f0cca5.chunk.js"
+  },
+  {
+    "revision": "378c4b7cc98c0b5ba4d1",
+    "url": "/static/js/runtime-main.5ea679ec.js"
+  }
+]);
